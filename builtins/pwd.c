6cd58569efd2d685d1bab776422b6edb773e4b00
Utharushan <Utharushan@gmail.com>
2025-04-30 14:01:04 +0200

    norminette

--- builtins/pwd.c
+++ builtins/pwd.c
@@ -3,30 +3,30 @@
 /*                                                        :::      ::::::::   */
 /*   pwd.c                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ebella <ebella@student.42.fr>              +#+  +:+       +#+        */
+/*   By: tuthayak <tuthayak@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/25 17:20:50 by ebella            #+#    #+#             */
-/*   Updated: 2025/03/25 17:35:23 by ebella           ###   ########.fr       */
+/*   Updated: 2025/04/30 13:40:23 by tuthayak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int     ft_pwd(void)
+int	ft_pwd(void)
 {
-    char    *cwd;
+	char	*cwd;
 
-    cwd = getcwd(NULL, 0);
-    if (cwd)
-    {
-        ft_putstr_fd(cwd, 1);
-        ft_putstr_fd("\n", 1);
-        free(cwd);
-        return (0);
-    }
-    else
-    {
-        perror("pwd");
-        return (1);
-    }
+	cwd = getcwd(NULL, 0);
+	if (cwd)
+	{
+		ft_putstr_fd(cwd, 1);
+		ft_putstr_fd("\n", 1);
+		free(cwd);
+		return (0);
+	}
+	else
+	{
+		perror("pwd");
+		return (1);
+	}
 }
