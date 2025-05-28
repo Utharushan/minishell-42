64b1204ba475ce2d313322b2b2c0020140e2cee1
Elbachir Bella <ebella@f2r6s15.clusters.42paris.fr>
2025-05-28 15:47:59 +0200

    j'ai avancer dans le export, a finir...

--- builtins/export.c
+++ builtins/export.c
@@ -6,38 +6,53 @@
 /*   By: ebella <ebella@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/25 13:09:43 by ebella            #+#    #+#             */
-/*   Updated: 2025/05/27 11:57:59 by ebella           ###   ########.fr       */
+/*   Updated: 2025/05/28 15:19:24 by ebella           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
-int	parse_arg(char *str)
+int	env_found(t_env *env, char *name)
 {
-	if (!str)
+	while (env)
 	{
-		perror("export");
-		return (1);
+		if (!ft_strcmp(name, env->name))
+			return (1);
+		env = env->next;
 	}
 	return (0);
 }
-
-int		ft_export(t_env *env, char *str)
+void	parse_arg(char *name, char *value, t_env *env)
 {
-	int i;
-	char	*name;
-	char	*value;
-	char	*equal_position;	
+	while (env)
+	{
+		if (!ft_strcmp(name, env->name))
+		{
+			env->value = value;
+			break ;
+		}
+		env = env->next;
+	}
+}
+
+int	ft_export(t_env *env, char *str)
+{
+	int		i;
 	t_env	*new_env;
+	char	*value;
+	char	*name;
+	char	*equal_position;
 
 	i = 0;
-	if (!env)
+	if (!env || !str)
 		return (1);
 	equal_position = ft_strchr(str, '=');
-	if (equal_position || parse_arg(str))
+	name = ft_substr(str, 0, equal_position - str);
+	value = ft_strdup(equal_position);
+	if (env_found(env, name) && equal_position)
+		parse_arg(name, value, env);
+	else
 	{
-		name = ft_substr(str, 0, equal_position - str);
-		value = ft_strdup(equal_position);
 		new_env = new_env_node(name, value);
 		add_env_back(&env, new_env);
 	}
