d87996b72c09d4ce52eaa99d49a75293a4af7590
Elba <elbachirbellaparcoursup@gmail.com>
2025-06-20 10:00:18 +0200

    giga functions norminée

--- src/exec/here_document.c
+++ src/exec/here_document.c
@@ -6,7 +6,7 @@
 /*   By: ebella <ebella@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/19 13:46:46 by ebella            #+#    #+#             */
-/*   Updated: 2025/06/17 17:17:25 by ebella           ###   ########.fr       */
+/*   Updated: 2025/06/18 10:52:27 by ebella           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,49 +24,73 @@ void heredoc_sigint(int sig)
 	ioctl(STDIN_FILENO, TIOCSTI, &c);
 }
 
+void setup_heredoc_signals(void)
+{
+    signal(SIGINT, heredoc_sigint);
+    signal(SIGQUIT, SIG_IGN);
+    g_signal_status = 0;
+}
+
+void restore_heredoc_signals(void)
+{
+    signal(SIGINT, sigint_handler);
+    signal(SIGQUIT, SIG_IGN);
+}
+
+int heredoc_write_line(char *line, int heredoc_expand, t_env *env, int fd)
+{
+    char *expanded;
+
+    if (heredoc_expand)
+    {
+        expanded = expand_token_value((char *)line, env, g_signal_status);
+        ft_putstr_fd(expanded, fd);
+        free(expanded);
+    }
+    else
+        ft_putstr_fd(line, fd);
+    ft_putchar_fd('\n', fd);
+    return (0);
+}
+
+int heredoc_loop(const char *delim, int heredoc_expand, t_env *env, int fd)
+{
+    char *line;
+
+    while (g_signal_status != 130)
+    {
+        line = readline("> ");
+        if (g_signal_status == 130 || !line || !ft_strcmp(line, delim))
+        {
+            free(line);
+            break;
+        }
+        heredoc_write_line(line, heredoc_expand, env, fd);
+        free(line);
+    }
+    return (0);
+}
+
 int here_doc(const char *delim, int heredoc_expand, t_env *env, t_command *cmds)
 {
-	int pipe_fd[2];
-	char *line;
-	char *expanded;
+    int pipe_fd[2];
 
-	if (pipe(pipe_fd) == -1)
-	{
-		perror("pipe");
-		return (0);
-	}
-	signal(SIGINT, heredoc_sigint);
-	signal(SIGQUIT, SIG_IGN);
-	g_signal_status = 0;
-	while (g_signal_status != 130)
-	{
-		line = readline("> ");
-		if (g_signal_status == 130 || !line || !ft_strcmp(line, delim))
-		{
-			free(line);
-			break ;
-		}
-		if (heredoc_expand)
-		{
-			expanded = expand_token_value(line, env, g_signal_status);
-			ft_putstr_fd(expanded, pipe_fd[1]);
-			free(expanded);
-		}
-		else
-			ft_putstr_fd(line, pipe_fd[1]);
-		ft_putchar_fd('\n', pipe_fd[1]);
-		free(line);
-	}
-	close(pipe_fd[1]);
-	signal(SIGINT, sigint_handler);
-	signal(SIGQUIT, SIG_IGN);
-	if (g_signal_status == 130)
-	{
-		cmds->status = 130;
-		close(pipe_fd[0]);
-		return (-1);
-	}
-	return (pipe_fd[0]);
+    if (pipe(pipe_fd) == -1)
+    {
+        perror("pipe");
+        return (0);
+    }
+    setup_heredoc_signals();
+    heredoc_loop(delim, heredoc_expand, env, pipe_fd[1]);
+    close(pipe_fd[1]);
+    restore_heredoc_signals();
+    if (g_signal_status == 130)
+    {
+        cmds->status = 130;
+        close(pipe_fd[0]);
+        return (-1);
+    }
+    return (pipe_fd[0]);
 }
 
 int prepare_heredocs(t_command *cmds, t_env *env)
